1ae96a6d57842189960620e20b2816913f53c61e
Igor Quiterio <idcq101@gmail.com>
2024-10-23 21:19:21 -0300

    feat: add global styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,4 @@
-import { styled } from '@/styles'
 import { HomeContainer } from '@/styles/pages/home'
-import localFont from 'next/font/local'
-import Head from 'next/head'
-import Image from 'next/image'
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-})
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-})
 
 export default function Home() {
   return (
